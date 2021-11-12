e723946def0d2853a3a92b2af5383c3a04fa90b9
gbernus <guillaume.bernus@cdiscount.com>
2021-11-12 10:02:23 +0100

    LawnFile.API lowcase urls

--- theHerbalizer/LawnFileAPI/Startup.cs
+++ theHerbalizer/LawnFileAPI/Startup.cs
@@ -29,7 +29,9 @@ namespace LawnFile.API
                    {
                        var enumConverter = new JsonStringEnumConverter();
                        opts.JsonSerializerOptions.Converters.Add(enumConverter);
+                       opts.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
                    });
+            services.AddRouting(options => options.LowercaseUrls = true);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "LawnFile.API", Version = "v1" });
@@ -42,13 +44,18 @@ namespace LawnFile.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+          
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LawnFile.API v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LawnFile.API v1"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
