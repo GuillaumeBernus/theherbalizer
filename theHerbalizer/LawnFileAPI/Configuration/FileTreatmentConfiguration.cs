c742987ebacb1223acf3c5b8e7f889bbd5589428
gbernus <guillaume.bernus@cdiscount.com>
2021-11-12 10:22:34 +0100

    Comments on LawnFile.API

--- theHerbalizer/LawnFileAPI/Configuration/FileTreatmentConfiguration.cs
+++ theHerbalizer/LawnFileAPI/Configuration/FileTreatmentConfiguration.cs
@@ -1,7 +1,14 @@
 ﻿namespace LawnFile.API.Configuration
 {
+    /// <summary>
+    /// Class FileTreatmentConfiguration.
+    /// </summary>
     public class FileTreatmentConfiguration
     {
+        /// <summary>
+        /// Gets or sets the temporary file directory path.
+        /// </summary>
+        /// <value>The temporary file directory path.</value>
         public string TemporaryFileDirectoryPath { get; set; }
     }
 }
--- theHerbalizer/LawnFileAPI/Controllers/LawnDescriptionFileController.cs
+++ theHerbalizer/LawnFileAPI/Controllers/LawnDescriptionFileController.cs
@@ -12,16 +12,44 @@ using System.Threading.Tasks;
 
 namespace LawnFile.API.Controllers
 {
+    /// <summary>
+    /// Class LawnDescriptionFileController.
+    /// Implements the <see cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
     [ApiController]
     [Route("[controller]")]
     public class LawnDescriptionFileController : ControllerBase
     {
+        /// <summary>
+        /// The logger
+        /// </summary>
         private readonly ILogger<LawnDescriptionFileController> _logger;
 
+        /// <summary>
+        /// The input file configuration
+        /// </summary>
         private readonly InputFileConfiguration _inputFileConfiguration;
+        /// <summary>
+        /// The file treatment configuration
+        /// </summary>
         private readonly FileTreatmentConfiguration _fileTreatmentConfiguration;
+        /// <summary>
+        /// The lawn file handler
+        /// </summary>
         private readonly ILawnFileHandler _lawnFileHandler;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LawnDescriptionFileController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="inputFileConfiguration">The input file configuration.</param>
+        /// <param name="fileTreatmentConfiguration">The file treatment configuration.</param>
+        /// <param name="lawnFileHandler">The lawn file handler.</param>
+        /// <exception cref="System.ArgumentNullException">logger</exception>
+        /// <exception cref="System.ArgumentNullException">inputFileConfiguration</exception>
+        /// <exception cref="System.ArgumentNullException">fileTreatmentConfiguration</exception>
+        /// <exception cref="System.ArgumentNullException">lawnFileHandler</exception>
         public LawnDescriptionFileController(ILogger<LawnDescriptionFileController> logger, IOptions<InputFileConfiguration> inputFileConfiguration
            , IOptions<FileTreatmentConfiguration> fileTreatmentConfiguration
             , ILawnFileHandler lawnFileHandler)
@@ -32,6 +60,11 @@ namespace LawnFile.API.Controllers
             _lawnFileHandler = lawnFileHandler ?? throw new ArgumentNullException(nameof(lawnFileHandler));
         }
 
+        /// <summary>
+        /// Post as an asynchronous operation.
+        /// </summary>
+        /// <param name="formFile">The form file.</param>
+        /// <returns>A Task&lt;ActionResult`1&gt; representing the asynchronous operation.</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -54,6 +87,11 @@ namespace LawnFile.API.Controllers
             }
         }
 
+        /// <summary>
+        /// Copy file as an asynchronous operation.
+        /// </summary>
+        /// <param name="formFile">The form file.</param>
+        /// <returns>A Task&lt;System.String&gt; representing the asynchronous operation.</returns>
         private async Task<string> CopyFileAsync(IFormFile formFile)
         {
             var filePath = Path.Combine(_fileTreatmentConfiguration.TemporaryFileDirectoryPath,
@@ -67,6 +105,11 @@ namespace LawnFile.API.Controllers
             return filePath;
         }
 
+        /// <summary>
+        /// Checks the file.
+        /// </summary>
+        /// <param name="formFile">The form file.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         private bool CheckFile(IFormFile formFile)
         {
             if (formFile.Length <= 0)
