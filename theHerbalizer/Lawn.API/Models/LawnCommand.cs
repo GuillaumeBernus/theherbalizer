0d54df686a722389468126f4e94dd88a76d92d89
gbernus <guillaume.bernus@cdiscount.com>
2021-11-13 15:58:11 +0100

    LawnCommandAssembler extraction

--- theHerbalizer/Lawn.API/Models/LawnCommand.cs
+++ theHerbalizer/Lawn.API/Models/LawnCommand.cs
@@ -1,9 +1,6 @@
-﻿using MowerEngine.Models;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
-using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -27,64 +24,5 @@ namespace Lawn.API.Models
         /// <value>The mowers.</value>
         [Required]
         public List<MowerViewModel> Mowers { get; set; }
-    }
-
-    public static class LawnCommandAssembler
-    {
-        public static MowerEngine.Models.Lawn ToLawn(LawnCommand viewModel)
-        {
-            if (viewModel == null)
-            {
-                return null;
-            }
-
-            return new MowerEngine.Models.Lawn().WithMowers(viewModel.Mowers.Select(m => ToMower(m)).ToList()).WithUpperRigthCorner(ToPoint(viewModel.UpperRigthCorner));
-        }
-
-        private static Mower ToMower(MowerViewModel viewModel)
-        {
-            if (viewModel == null)
-            {
-                return null;
-            }
-
-            return new Mower
-            {
-                Position = ToMowerPosition(viewModel.StartPosition)
-                ,
-                Route = viewModel.Route
-            };
-        }
-
-        private static MowerPosition ToMowerPosition(MowerPositionViewModel viewModel)
-        {
-            if (viewModel == null)
-            {
-                return null;
-            }
-            if (!Enum.TryParse<Direction>(viewModel.Orientation, out Direction orientation))
-            {
-                return null;
-            }
-
-            return new MowerPosition
-            {
-                Coordinates = ToPoint(viewModel.Coordinates),
-                Orientation = orientation
-            };
-        }
-
-        public static Point ToPoint(PointViewModel viewModel)
-        {
-            if (viewModel == null)
-            {
-                return null;
-            }
-            return new Point
-            {
-                X = viewModel.X,
-                Y = viewModel.Y
-            };
-        }
     }
 }
---
+++ theHerbalizer/Lawn.API/Models/LawnCommandAssembler.cs
@@ -0,0 +1,65 @@
+﻿using MowerEngine.Models;
+using System;
+using System.Linq;
+
+namespace Lawn.API.Models
+{
+    public static class LawnCommandAssembler
+    {
+        public static MowerEngine.Models.Lawn ToLawn(LawnCommand viewModel)
+        {
+            if (viewModel == null)
+            {
+                return null;
+            }
+
+            return new MowerEngine.Models.Lawn().WithMowers(viewModel.Mowers.Select(m => ToMower(m)).ToList()).WithUpperRigthCorner(ToPoint(viewModel.UpperRigthCorner));
+        }
+
+        private static Mower ToMower(MowerViewModel viewModel)
+        {
+            if (viewModel == null)
+            {
+                return null;
+            }
+
+            return new Mower
+            {
+                Position = ToMowerPosition(viewModel.StartPosition)
+                ,
+                Route = viewModel.Route
+            };
+        }
+
+        private static MowerPosition ToMowerPosition(MowerPositionViewModel viewModel)
+        {
+            if (viewModel == null)
+            {
+                return null;
+            }
+            if (!Enum.TryParse<Direction>(viewModel.Orientation, out Direction orientation))
+            {
+                return null;
+            }
+
+            return new MowerPosition
+            {
+                Coordinates = ToPoint(viewModel.Coordinates),
+                Orientation = orientation
+            };
+        }
+
+        public static Point ToPoint(PointViewModel viewModel)
+        {
+            if (viewModel == null)
+            {
+                return null;
+            }
+            return new Point
+            {
+                X = viewModel.X,
+                Y = viewModel.Y
+            };
+        }
+    }
+}
